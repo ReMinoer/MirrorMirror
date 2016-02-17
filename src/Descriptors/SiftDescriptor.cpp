9eee7bae11841c57481b39a926c16a660d823204
Valentin Roucoux <12019373@e103m08.istic.univ-rennes1.fr>
2016-02-17 15:55:27 +0100

    addDescriptors

--- src/Descriptors/SiftDescriptor.cpp
+++ src/Descriptors/SiftDescriptor.cpp
@@ -2,21 +2,44 @@
 
 SiftDescriptor::SiftDescriptor()
 {
+	std::vector<KeyPoint> _keyPoints();
 }
 
 void SiftDescriptor::compute(const Mat &image)
 {
-	
+	SIFT descri();
+	descri()(image,cv::noArray(),_keyPoints,cv::noArray());
 }
 
 string SiftDescriptor::getData() const
 {
-	return "";
+	stringstream dataKeyPoints;
+	dataKeyPoints << _keyPoints.size() << ";";
+	for(int i=0;i<_keyPoints.size();i++)
+	{
+		dataKeyPoints << _keyPoints[i].pt.x << "," << _keyPoints[i].pt.y << "," << _keyPoints[i].size << ";";
+	}
+	//dataKeyPoints << "!";
+	return dataKeyPoints.str();
 }
 
-void SiftDescriptor::setData(string data) const
+void SiftDescriptor::setData(string data) 
 {
-	
+	_keyPoints.clear();
+	stringstream dataKeyPoints(data);
+	int nbPoints;
+	dataKeyPoints >> nbPoints >> ";";
+	for (int i = 0; i < nbPoints; i++)
+	{
+		float x, y, size;
+		dataKeyPoints >> x >> "," >> y >> "," >> size >> ";";
+		_keyPoints.push_back(KeyPoint(x,y,size));
+/*
+string dataS = data.substr(maPos,data.find(";"));
+		_keyPoints.push_back(dataS);
+		maPos=data.find(";")+1;
+*/
+	}
 }
 
 const vector<KeyPoint>& SiftDescriptor::getKeyPoints() const
--- src/Descriptors/SiftDescriptor.hpp
+++ src/Descriptors/SiftDescriptor.hpp
@@ -3,6 +3,7 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
+#include </share/esir3/opencv-2.4.9/include/opencv2/nonfree/features2d.hpp>
 #include <vector>
 #include "DescriptorBase.hpp"
 
@@ -16,7 +17,7 @@ class SiftDescriptor : public DescriptorBase
 		SiftDescriptor();
 		virtual void compute(const Mat &image);
 		virtual string getData() const;
-		virtual void setData(string data) const;
+		virtual void setData(string data) ;
 		virtual const vector<KeyPoint>& getKeyPoints() const;
 };
 
